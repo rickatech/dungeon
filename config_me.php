ae885ef82809da90226c0998b6c743f04b03bcfe
Rick Armstrong <rickatech@c6x4ip89.local.zaptech.org>
2015-02-09 18:57:22 -0800

    added some more debug cnostants

--- config_me.php
+++ config_me.php
@@ -4,8 +4,26 @@ $isprod = false;
 $title =  'Dungeon Test'
 
 //  uncomment to enable various debug output
-//  const DEBUG_FOO =   1;
-//  const DEBUG_USR =  2;
-//  const DEBUG_FRM = 4;
-$debug_mask = DEBUG_FOO | DEBUG_USR | DEBUG_FRM | 0; 
+const DEBUG_FOO = 1;
+const DEBUG_USR =  2;
+const DEBUG_FRM =   4;
+const DEBUG_ADM =    8;         //  rickatech extra status
+const DEBUG_KEY =    16;        //  experiment for direct keyboard control
+//$debug_mask = DEBUG_ADM; 
+$debug_mask = 0; 
+
+//  global, room, and 1:1 chat, fee or reputation needed
+//  global_chat
+
+//  'local' chat map file suffix
+//  _chat
+
+//  global trade log: item trades
+//  trade_chat
+
+//  action log map file suffix: player tags, player kicks
+//  _log
+
+//  available non-player maps
+//  test
 ?>
